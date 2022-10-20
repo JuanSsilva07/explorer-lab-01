339f46d8662ad8da8757b1365d51043fdeb83b33
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-10-20 15:41:39 -0300

    add american-express

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ function setCardType(type) {
   const colors = {
     visa: ["#2D57F2", "#436D99"],
     mastercard: ["#DF6F29", "#C69347"],
+    americanexpress: ["#213239", "#3590B5"],
     default: ["black", "gray"],
   }
   ccColor01.setAttribute("fill", colors[type][0])
@@ -52,19 +53,9 @@ const cardNumber = document.querySelector("#card-number")
 const cardNumberPattern = {
   mask: [
     {
-      mask: "0000 000000 00000",
-      regex: /^3[47]\d{0,13}/,
-      cardtype: "american express",
-    },
-    {
       mask: "0000 0000 0000 0000",
-      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
-      cardtype: "discover",
-    },
-    {
-      mask: "0000 000000 0000",
-      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
-      cardtype: "diners",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
     },
     {
       mask: "0000 0000 0000 0000",
@@ -73,28 +64,12 @@ const cardNumberPattern = {
     },
     {
       mask: "0000 000000 00000",
-      regex: /^(?:2131|1800)\d{0,11}/,
-      cardtype: "jcb15",
+      regex: /^3[4-7]\d{0,13}/,
+      cardtype: "americanexpress",
     },
     {
       mask: "0000 0000 0000 0000",
-      regex: /^(?:35\d{0,2})\d{0,12}/,
-      cardtype: "jcb",
-    },
-    {
-      mask: "0000 0000 0000 0000",
-      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
-      cardtype: "maestro",
-    },
-    {
-      mask: "0000 0000 0000 0000",
-      regex: /^4\d{0,15}/,
-      cardtype: "visa",
-    },
-    {
-      mask: "0000 0000 0000 0000",
-      regex: /^62\d{0,14}/,
-      cardtype: "unionpay",
+      cardtype: "default",
     },
   ],
   dispatch: function (appended, dynamicMasked) {
@@ -149,24 +124,68 @@ cardNumberMasked.on("accept", () => {
   updateCardNumber(cardNumberMasked.value)
 })
 
-function updateCardNumber(number) {
+function updateCardNumber(number){
   const ccNumber = document.querySelector(".cc-number")
   ccNumber.innerText = number.length === 0 ? "1234 5678 9123 4567" : number
 }
 
 /* Capturando e inserindo conteúdo da data de expiração do cartão */
 
-expirationDateMasked.on("accept", () => {
+expirationDateMasked.on("accept", () =>{
   updateExpirationDate(expirationDateMasked.value)
 })
 
-function updateExpirationDate(date) {
+function updateExpirationDate(date){
   const ccExpiration = document.querySelector(".cc-extra .value")
   ccExpiration.innerText = date.length === 0 ? "02/32" : date
 }
 
 /* 
 const cardsDynamicMasks = [
-
+    {
+        mask: '0000 000000 00000',
+        regex: /^3[47]\d{0,13}/,
+        cardtype: 'american express'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+        cardtype: 'discover'
+    },
+    {
+        mask: '0000 000000 0000',
+        regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+        cardtype: 'diners'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+        cardtype: 'mastercard'
+    },
+    {
+        mask: '0000 000000 00000',
+        regex: /^(?:2131|1800)\d{0,11}/,
+        cardtype: 'jcb15'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^(?:35\d{0,2})\d{0,12}/,
+        cardtype: 'jcb'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+        cardtype: 'maestro'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^4\d{0,15}/,
+        cardtype: 'visa'
+    },
+    {
+        mask: '0000 0000 0000 0000',
+        regex: /^62\d{0,14}/,
+        cardtype: 'unionpay'
+    },
 ]
 */
