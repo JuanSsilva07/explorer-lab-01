19554d44f2e01aab25b2a4797d2bc7f805508e79
Juan Soares Silva <70165034+JuanSsilva07@users.noreply.github.com>
2022-10-20 14:40:44 -0300

    adicionando mais cartões

--- src/main.js
+++ src/main.js
@@ -52,9 +52,19 @@ const cardNumber = document.querySelector("#card-number")
 const cardNumberPattern = {
   mask: [
     {
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "american express",
+    },
+    {
       mask: "0000 0000 0000 0000",
-      regex: /^4\d{0,15}/,
-      cardtype: "visa",
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardtype: "discover",
+    },
+    {
+      mask: "0000 000000 0000",
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardtype: "diners",
     },
     {
       mask: "0000 0000 0000 0000",
@@ -62,8 +72,29 @@ const cardNumberPattern = {
       cardtype: "mastercard",
     },
     {
+      mask: "0000 000000 00000",
+      regex: /^(?:2131|1800)\d{0,11}/,
+      cardtype: "jcb15",
+    },
+    {
       mask: "0000 0000 0000 0000",
-      cardtype: "default",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardtype: "jcb",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^62\d{0,14}/,
+      cardtype: "unionpay",
     },
   ],
   dispatch: function (appended, dynamicMasked) {
@@ -118,18 +149,24 @@ cardNumberMasked.on("accept", () => {
   updateCardNumber(cardNumberMasked.value)
 })
 
-function updateCardNumber(number){
+function updateCardNumber(number) {
   const ccNumber = document.querySelector(".cc-number")
   ccNumber.innerText = number.length === 0 ? "1234 5678 9123 4567" : number
 }
 
 /* Capturando e inserindo conteúdo da data de expiração do cartão */
 
-expirationDateMasked.on("accept", () =>{
+expirationDateMasked.on("accept", () => {
   updateExpirationDate(expirationDateMasked.value)
 })
 
-function updateExpirationDate(date){
+function updateExpirationDate(date) {
   const ccExpiration = document.querySelector(".cc-extra .value")
   ccExpiration.innerText = date.length === 0 ? "02/32" : date
 }
+
+/* 
+const cardsDynamicMasks = [
+
+]
+*/
